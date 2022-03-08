4477e766b3d840bc058b3789483e5da2b14cc061
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-03-09 05:07:45 +0900

    updated next@12.1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,35 +16,12 @@ module.exports = {
           DEFAULT: '#ff0000',
         },
         blue: {
-          DEFAULT: '#556EE6',
+          DEFAULT: '#0000ff',
         },
-        purple: {
-          DEFAULT: '#6B21A8',
-        },        
+        green: {
+          DEFAULT: '#00ff00',
+        },
       },
-      width:{
-        '300':'300px',
-        '400':'400px',
-      }
-    },
-    bottom: {
-      0: 0,
-      75: '75px',
-    },
-    fontSize: {
-      8: ['8px', '14px'],
-      xs: ['10px', '18px'],
-      12: ['12px', '18px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      18: ['18px', '26px'],
-      lg: ['20px', '28px'],
-      22: ['22px', '34px'],
-      xl: ['24px', '36px'],
-      28: ['28px', '36px'],
-      32: ['32px', '40px'],
-      36: ['36px', '52px'],
-      40: ['40px', '50px'],
     },
     fontWeight: {
       hairline: 100,
